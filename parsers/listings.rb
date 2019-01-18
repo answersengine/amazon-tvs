2d37233be6d057e53c5c7d23d0dd863ada1ec938
David Lynam <dlynam@gmail.com>
2019-01-17 16:38:01 -0800

    add listings parser

---
+++ parsers/listings.rb
@@ -0,0 +1,27 @@
+nokogiri = Nokogiri.HTML(content)
+
+#load products
+products = nokogiri.css('#mainResults li')
+products.each do |product|
+  url = product.at_css('a.s-access-detail-page')['href']
+  pages << {
+      url: url,
+      page_type: 'products',
+      vars: {
+        category: page['vars']['category']
+      }
+    }
+end
+
+#load paginated links
+pagination_links = nokogiri.css('#pagn a')
+pagination_links.each do |link|
+  url = URI.join('https://www.amazon.com', link['href']).to_s
+  pages << {
+      url: url,
+      page_type: 'listings',
+      vars: {
+        category: page['vars']['category']
+      }
+    }
+end
