bc2713314a7c3b9933442e708dd2295a01aaacb6
David Lynam <dlynam@gmail.com>
2019-01-31 15:07:47 -0800

    update listings parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -22,12 +22,15 @@ end
 #load paginated links
 pagination_links = nokogiri.css('#pagn a')
 pagination_links.each do |link|
-  url = URI.join('https://www.amazon.com', link['href']).to_s.gsub(/&qid=[0-9]*/,'')
-  pages << {
-      url: url,
-      page_type: 'listings',
-      vars: {
-        category: page['vars']['category']
+  page_num = link.text.strip
+  if page_num =~ /[0-9]/
+    url = "https://www.amazon.com/s/ref=sr_pg_3?rh=n%3A172282%2Cn%3A%21493964%2Cn%3A1266092011%2Cn%3A172659%2Cn%3A6459737011&page=#{page_num}&ie=UTF8"
+    pages << {
+        url: url,
+        page_type: 'listings',
+        vars: {
+          category: page['vars']['category']
+        }
       }
-    }
+  end
 end
